dbde5df2ec90758d4c3d77f9ecc82040d0ef291e
Nagabhushan M D <45279279+Nagabushangowdamd@users.noreply.github.com>
2019-06-13 19:41:37 +0530

    Add files via upload

---
+++ iamderMVP/src/main/java/com/iamder/IamderMvpApplication.java
@@ -0,0 +1,13 @@
+package com.iamder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamderMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamderMvpApplication.class, args);
+	}
+
+}
